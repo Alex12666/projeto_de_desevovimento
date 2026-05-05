fae2904f99c6427de327578ed5d6ac7fd4a93d27
Alexandre <jmalex321@gmail>
2026-05-05 17:28:56 -0300

    seguraça

--- terraform/segure.tf
+++ terraform/segure.tf
@@ -1,14 +1,25 @@
+# 1. Regra para SSH (Sua porta de entrada para o terminal)
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0" 
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# 2. Regra para HTTP (Porta 80 - Para o site rodar direto pelo IP)
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0" # Aberto para toda a internet
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
 
+# 3. Regra para porta 8080 (Onde seu container está agora)
 resource "aws_vpc_security_group_ingress_rule" "allow_8080_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0" # Aberto para toda a internet
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
   ip_protocol       = "tcp"
   to_port           = 8080
